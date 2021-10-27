0a40296e5dd2867a6695712405e9de4e726b9431
ethanloseweight <62718132+ethanloseweight@users.noreply.github.com>
2021-10-26 18:21:09 -0700

    Update tester.cpp

--- tester.cpp
+++ tester.cpp
@@ -1,81 +1,64 @@
-// SmartHDD.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "SmartReader.h"
-#include "Common.h"
-#include "CTracer.h"
-
-CTracer g_objTracer(L"", L"SmartHDD.log");
 
 int main()
 {
-	DF_TRACE_LOG(L"main() - ENTER");
+    CSmartReader objSmartReader;
 
-	CSmartReader objSmartReader;
+    BYTE ucT1 = 0;
+    BYTE ucT2 = 0;
+    BYTE ucT3 = 0;
 
-	BYTE ucT1 = 0;
-	BYTE ucT2 = 0;
-	BYTE ucT3 = 0;
-	BYTE ucT4 = 0;
+    ST_DRIVE_INFO* pDriveInfo = NULL;
+    ST_SMART_INFO* pSmartInfo = NULL;
+    ST_SMART_DETAILS* pSmartDetails = NULL;
+    ST_IDSECTOR* pSectorInfo = NULL;
 
-	ST_DRIVE_INFO* pDriveInfo = NULL;
-	ST_SMART_INFO* pSmartInfo = NULL;
-	ST_SMART_DETAILS* pSmartDetails = NULL;
-	ST_IDSECTOR* pSectorInfo = NULL;
+    char szT1[MAX_PATH] = { 0 };
 
-	char szT1[MAX_PATH];
+    // Reading Self-Monitoring Analysis and Reporting Technology (SMART) Values
 
-	DF_TRACE_LOG(L"main() - Call ReadSMARTValuesForAllDrives()");
+    objSmartReader.ReadSMARTValuesForAllDrives();
 
-	// Reading Self-Monitoring Analysis and Reporting Technology (SMART) Values
-	objSmartReader.ReadSMARTValuesForAllDrives();
+    for (ucT1 = 0; ucT1 < objSmartReader.m_ucDrivesWithInfo; ++ucT1)
+    {
+        wsprintf(szT1, "");
+        std::cout << szT1 << std::endl;
 
-	DF_TRACE_LOG(L"main() - m_ucDrivesWithInfo = %d", objSmartReader.m_ucDrivesWithInfo);
+        wsprintf(szT1, ">>>>>>>>>>>>>>>  Drive %d  <<<<<<<<<<<<<<<<", ucT1);
+        std::cout << szT1 << std::endl;
 
-	for (ucT1 = 0, ucT4 = 0; ucT1 < objSmartReader.m_ucDrivesWithInfo; ++ucT1)
-	{
-		wsprintf(szT1, "");
-		std::cout << szT1 << std::endl;
+        pSectorInfo = &objSmartReader.m_stDrivesInfo[ucT1].m_stInfo;
 
-		wsprintf(szT1, ">>>>>>>>>>>>>>>  Drive %d  <<<<<<<<<<<<<<<<", ucT1);
-		std::cout << szT1 << std::endl;
+        std::cout << "Model:  " << pSectorInfo->sModelNumber << std::endl;
 
-		pSectorInfo = &objSmartReader.m_stDrivesInfo[ucT1].m_stInfo;
+        std::cout << "Serial: " << pSectorInfo->sSerialNumber << std::endl;
 
-		std::cout << "Model:  " << pSectorInfo->sModelNumber << std::endl;
+        //std::cout << "Cache = " << pSectorInfo->wBufferSize << std::endl;
 
-		std::cout << "Serial: " << pSectorInfo->sSerialNumber << std::endl;
+        //std::cout << "Buffer = " << pSectorInfo->wBufferType << std::endl;
 
-		std::cout << "Cache = " << pSectorInfo->wBufferSize << std::endl;
+        pDriveInfo = objSmartReader.GetDriveInfo(ucT1);
 
-		std::cout << "Buffer = " << pSectorInfo->wBufferType << std::endl;
+        //for (ucT2 = 0; ucT2 < 255; ++ucT2)
+        for (ucT2 = 196; ucT2 < 199; ++ucT2)
+        {
+            pSmartInfo = objSmartReader.GetSMARTValue(pDriveInfo->m_ucDriveIndex, ucT2);
 
-		//wsprintf(szT1, "Cache=%d Buffer=%d", pSectorInfo->wBufferSize, pSectorInfo->wBufferType);
+            if (pSmartInfo)
+            {
+                pSmartDetails = objSmartReader.GetSMARTDetails(pSmartInfo->m_ucAttribIndex);
 
-		//std::cout << szT1 << std::endl;
+                if (pSmartDetails)
+                {
+                    if (pSmartDetails->m_bCritical)
+                        wsprintf(szT1, "(CRITICAL) ATTRIBUTE Name=%s Id=%d Value=%d Worst=%d AttributeValue=%lu Threshold=%lu", pSmartDetails->m_csAttribName.GetString(), pSmartDetails->m_ucAttribId, pSmartInfo->m_ucValue, pSmartInfo->m_ucWorst, pSmartInfo->m_dwAttribValue, pSmartInfo->m_dwThreshold);
+                    else
+                        wsprintf(szT1, "ATTRIBUTE Name=%s Id=%d Value=%d Worst=%d AttributeValue=%lu Threshold=%lu", pSmartDetails->m_csAttribName.GetString(), pSmartDetails->m_ucAttribId, pSmartInfo->m_ucValue, pSmartInfo->m_ucWorst, pSmartInfo->m_dwAttribValue, pSmartInfo->m_dwThreshold);
 
-		pDriveInfo = objSmartReader.GetDriveInfo(ucT1);
-
-		for (ucT2 = ucT3 = 0; ucT2 < 255; ++ucT2)
-		{
-			pSmartInfo = objSmartReader.GetSMARTValue(pDriveInfo->m_ucDriveIndex, ucT2);
-			if (pSmartInfo)
-			{
-				pSmartDetails = objSmartReader.GetSMARTDetails(pSmartInfo->m_ucAttribIndex);
-
-				if (pSmartDetails)
-				{
-					if (pSmartDetails->m_bCritical)
-						wsprintf(szT1, "(CRITICAL) ATTRIBUTE Name=%s Id=%d Value=%d Worst=%d AttributeValue=%lu Threshold=%lu", pSmartDetails->m_csAttribName.GetString(), pSmartDetails->m_ucAttribId, pSmartInfo->m_ucValue, pSmartInfo->m_ucWorst, pSmartInfo->m_dwAttribValue, pSmartInfo->m_dwThreshold);
-					else
-						wsprintf(szT1, "ATTRIBUTE Name=%s Id=%d Value=%d Worst=%d AttributeValue=%lu Threshold=%lu", pSmartDetails->m_csAttribName.GetString(), pSmartDetails->m_ucAttribId, pSmartInfo->m_ucValue, pSmartInfo->m_ucWorst, pSmartInfo->m_dwAttribValue, pSmartInfo->m_dwThreshold);
-
-					std::cout << szT1 << std::endl;
-
-					ucT4++;
-				}
-			}
-		}
-	}
+                    std::cout << szT1 << std::endl;
+                }
+            }
+        }
+    }
 }
